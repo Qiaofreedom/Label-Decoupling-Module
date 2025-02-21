949256edf9c02e0f0ae7ad37252437c1b4783bd3
Qiaofreedom <103122122+Qiaofreedom@users.noreply.github.com>
2025-02-21 15:10:10 +0100

    Update basic_conv1d_decoupled.py

--- code_used_upload/models/basic_conv1d_decoupled.py
+++ code_used_upload/models/basic_conv1d_decoupled.py
@@ -283,7 +283,7 @@ def create_head1d_decoupled(nf:int, nc:int, lin_ftrs:Optional[Collection[int]]=N
     "Model head that takes `nf` features, runs through `lin_ftrs`, and about `nc` classes; added bn and act here"
     lin_ftrs = [2*nf if concat_pooling else nf, nc] if lin_ftrs is None else [2*nf if concat_pooling else nf] + lin_ftrs + [nc]  # was [nf, 512,nc]   # lin_ftrs 确定了 传统的head的 倒数第二层的 特征数量（论文里面的P值）。
     div_lin_ftrs = [2*nf if concat_pooling else nf, nc] if div_lin_ftrs is None else [2*nf if concat_pooling else nf] + div_lin_ftrs + [1] #was [nf, 512, 1]   # div_lin_ftrs定义了 head部分 加入了LDM结构的 separation layer层。包括输入维度、隐藏层结构和最终输出维度。
-    # 如果 div_lin_ftrs=[512, 256]，那么 separation layer 结构就是：  输入维度 → 512 → 256 → 1（最终输出）
+    # 如果 div_lin_ftrs=[512, 256]，那么 separation layer 结构就是：  输入维度 → 512 → 256 → 1（1指的是最终输出维度，通道数）
     ps = listify(ps)
     if len(ps)==1: ps = [ps[0]/2] * (len(lin_ftrs)-2) + ps
     em_actns = [nn.ReLU(inplace=True) if act == "relu" else nn.ELU(inplace=True)] * (len(lin_ftrs) - 2) + [None] # 基本模型里面的 激活函数
